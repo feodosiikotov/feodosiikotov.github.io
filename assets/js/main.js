800c09dc320bde22923608ae6fd0625fd88b474e
feodosiikotov <47477519+feodosiikotov@users.noreply.github.com>
2020-06-17 15:06:22 +0300

    deleted pictures, added mail, pdf and links to github

--- assets/js/main.js
+++ assets/js/main.js
@@ -399,3 +399,11 @@
 					});
 
 })(jQuery);
+
+function copyToClipboard(element) {
+	var $temp = $("<input>");
+	$("body").append($temp);
+	$temp.val($(element).text()).select();
+	document.execCommand("copy");
+	$temp.remove();
+  }
